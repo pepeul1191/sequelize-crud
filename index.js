855adffcfe511f3c5183a0a1cb6a8f83d4679dbb
Pepe Valdivia <jvaldivia@softweb.pe>
2019-07-20 22:40:21 -0500

    Crear

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const models = require('./configs/models');
 
 async function listar(){
   var students = await models.Student.findAll({});
+  /*
   students.forEach(function(student) {
     console.log(JSON.stringify(student));
   });
+  */
   console.log(JSON.stringify(students));
-  return '';
+}
+
+async function crear(name, code){
+  var new_student;
+  try{
+    new_student= await models.Student.create({
+      name: name,
+      code: code,
+    });  
+  }catch(error){
+    console.log(error);
+  }
+  console.log(JSON.stringify(new_student));
 }
 
 listar();
-
+// crear('Carlos', 'l21j312');
